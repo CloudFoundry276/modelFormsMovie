9fd6b360cd5b2ae1a212d0baba86c67245b1d594
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-18 02:26:35 +0530

    model form movie project done

--- modelFormsMovieApp/views.py
+++ modelFormsMovieApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from modelFormsMovieApp.forms import MovieForm
+from modelFormsMovieApp.models import Movie
+
 
 # Create your views here.
+def index(request):
+    return render(request, "modelFormsMovieApp/index.html")
+
+
+def listMovies(request):
+    movies_list = Movie.objects.all()
+    return render(request, "modelFormsMovieApp/movies_list.html", {"movies": movies_list})
+
+
+def addMovie(request):
+    form = MovieForm()
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    return render(request, "modelFormsMovieApp/movie_add.html", {"form": form})
